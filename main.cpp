40d3ed5b1b3f4e8bfeccaf121c188ee159872f3a
lukexue07 <lxue1@ufl.edu>
2025-04-12 22:44:06 -0400

    Basic setup

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by lukel on 4/12/2025.
+//
+
+#include <string>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //read in the words from the text file and store it
+    //prompt user for their word search grid
+    //bfs+dfs searches
+}
